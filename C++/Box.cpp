ed2754bd761cd96878c96bc6cf67b10fa602f778
mihailnour <145534021+mihailnour@users.noreply.github.com>
2024-05-22 23:02:28 +0600

    Update Box.cpp

--- C++/Box.cpp
+++ C++/Box.cpp
@@ -1,99 +1,83 @@
-#include <iostream>
+#include "Box.h"
 
-struct Box {
-    int length;
-    int width;
-    int height;
-    double weight;
-    int value;
+namespace Storage {
 
-    Box(int l, int w, int h, double wei, int val) {
-        length = l;
-        width = ;
-        height = h;
-        weight = wei;
-        value = val;
-    }
-};
+Box::Box(int l, int w, int h, double wt, int val)
+    : length(l), width(w), height(h), weight(wt), value(val) {}
 
-std::ostream& operator<<(std::ostream& os, const Box& box) {
-    os << "Length: " << box.length << ", Width: " << box.width << ", Height: " << box.height << ", Weight: " << box.weight << ", Value: " << box.value;
-    return os;
+int Box::getLength() const { return length; }
+int Box::getWidth() const { return width; }
+int Box::getHeight() const { return height; }
+double Box::getWeight() const { return weight; }
+int Box::getValue() const { return value; }
+
+void Box::setLength(int l) { length = l; }
+void Box::setWidth(int w) { width = w; }
+void Box::setHeight(int h) { height = h; }
+void Box::setWeight(double wt) { weight = wt; }
+void Box::setValue(int val) { value = val; }
+
+bool Box::operator==(const Box &other) const {
+    return length == other.length && width == other.width &&
+           height == other.height && weight == other.weight &&
+           value == other.value;
 }
 
-std::istream& operator >> (std::istream& is, Box& box) {
-    is >> box.length >> box.width >> box.height >> box.weight >> box.value;
-    return is;
-}
-    bool operator==(const Box& a, const Box& b) {
-        return a.length == b.length && a.width == b.width && a.height == b.height && a.weight == b.weight && a.value == b.value;
-    }
-};
-
-int calculateTotalValue(const Box* boxes, int size) {
-    int totalValue = 0;
-    for (int i = 0; i < size; i++) {
-        totalValue += boxes[i].value;
-    }
-    return totalValue;
+std::ostream& operator<<(std::ostream &out, const Box &box) {
+    out << "Box(length: " << box.length << ", width: " << box.width
+        << ", height: " << box.height << ", weight: " << box.weight
+        << ", value: " << box.value << ")";
+    return out;
 }
 
-bool compareTotalDimensions(const Box* boxes, int size, int maxTotalDim) {
-    int TotalDim = 0;
-    for (int i = 0; i < size; i++) {
-        TotalDim += boxes[i].length + boxes[i].width + boxes[i].height;
-    }
-    return TotalDim <= maxTotalDim;
+std::istream& operator>>(std::istream &in, Box &box) {
+    in >> box.length >> box.width >> box.height >> box.weight >> box.value;
+    return in;
 }
 
-double getMaxWeight(const Box* boxes, int size, double maxV) {
-    double maxWeight = 0.0;
+int totalValue(Box boxes[], int size) {
+    int total = 0;
     for (int i = 0; i < size; ++i) {
-        double volume = boxes[i].length * boxes[i].width * boxes[i].height;
-        if (volume <= maxV && boxes[i].weight > maxWeight) {
-            maxWeight = boxes[i].weight;
+        total += boxes[i].getValue();
+    }
+    return total;
+}
+
+bool checkDimensions(Box boxes[], int size, int maxDim) {
+    int totalDim = 0;
+    for (int i = 0; i < size; ++i) {
+        totalDim += boxes[i].getLength() + boxes[i].getWidth() + boxes[i].getHeight();
+    }
+    return totalDim <= maxDim;
+}
+
+double maxWeightUnderVolume(Box boxes[], int size, int maxV) {
+    double maxWeight = 0;
+    for (int i = 0; i < size; ++i) {
+        int volume = boxes[i].getLength() * boxes[i].getWidth() * boxes[i].getHeight();
+        if (volume <= maxV && boxes[i].getWeight() > maxWeight) {
+            maxWeight = boxes[i].getWeight();
         }
     }
     return maxWeight;
 }
 
-bool canFitInside(const Box* boxes, int size) {
-    for (int i = 0; i < size; ++i) {
+bool canNest(Box boxes[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        bool canNestCurrent = false;
         for (int j = 0; j < size; ++j) {
-            if (i != j) {
-                if (boxes[i].length >= boxes[j].length && boxes[i].width >= boxes[j].width && boxes[i].height >= boxes[j].height) {
-                    continue;
-                }
-                else {
-                    return false;
-                }
+            if (i != j && boxes[i].getLength() < boxes[j].getLength() &&
+                boxes[i].getWidth() < boxes[j].getWidth() &&
+                boxes[i].getHeight() < boxes[j].getHeight()) {
+                canNestCurrent = true;
+                break;
             }
+        }
+        if (!canNestCurrent) {
+            return false;
         }
     }
     return true;
 }
 
-int main() {
-    Box box1(10, 5, 8, 2.5, 200);
-    Box box2(8, 6, 4, 1.8, 150);
-    Box boxesArray[] = { box1, box2 };
-    bool isTotalDimAccepted = compareTotalDimensions(boxesArray, 2, 50);
-    double maxWeightForMaxVolume = getMaxWeight(boxesArray, 2, 100.0);
-    int totalValue = calculateTotalValue(boxesArray, 2);
-    bool allFitInside = canFitInside(boxesArray, 2);
-    std::cout << "Total value of all boxes: " << totalValue << " kopecks" << std::endl;
-    if (isTotalDimAccepted) {
-        std::cout << "Total dimensions of all boxes are within the limit." << std::endl;
-    }
-    else {
-        std::cout << "Total dimensions of all boxes exceed the limit." << std::endl;
-    }
-    std::cout << "Maximum weight for boxes with volume <= 100: " << maxWeightForMaxVolume << " kg" << std::endl;
-    if (allFitInside) {
-        std::cout << "All boxes can fit inside each other." << std::endl;
-    }
-    else {
-        std::cout << "Not all boxes can fit inside each other." << std::endl;
-    }
-    return 0;
-}
+} // namespace Storage
